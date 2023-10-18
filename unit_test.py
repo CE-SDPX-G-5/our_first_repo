d7ca30cddda9f5230104fd52f508b6ab9d217460
Pom <95214492+Jintawat-Pom@users.noreply.github.com>
2023-10-18 21:37:44 +0700

    Add files via upload

--- unit_test.py
+++ unit_test.py
@@ -1,17 +1,26 @@
 import unittest
-
-from app import app
+import app
 
 class AppTestCase(unittest.TestCase):
-    def test_hello_with_string(self):
-        # test hello
-        res = app.hello("Rathachai")
-        self.assertEqual(res, "Hello, Rathachai")
+    def test_7(self):
+        res = app.next2('5')
+        data = res.get_json()
+        self.assertEqual(data['mrg'], 7)
 
-    def test_hello_with_number(self):
-        # test hello
-        res = app.hello(1)
-        self.assertEqual(res, "Hello, 1")
+    def test_8(self):
+        res = app.next2('6')
+        data = res.get_json()
+        self.assertEqual(data['mrg'], 8)
+
+    def test_8(self):
+        res = app.next2('3.5')
+        data = res.get_json()
+        self.assertEqual(data['mrg'], 5.5)
+
+    def test_8(self):
+        res = app.next2('a')
+        data = res.get_json()
+        self.assertEqual(data['mrg'], 'Error')
 
 if __name__ == "__main__":
     unittest.main()
