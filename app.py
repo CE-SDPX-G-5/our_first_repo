611053f9ad2249d697e507420f3bb0f878b5c979
64015168 <98441711+64015168@users.noreply.github.com>
2023-10-19 13:44:55 +0700

    exam

--- app.py
+++ app.py
@@ -1,33 +1,40 @@
+from flask import Flask,jsonify
 
-from flask import Flask, jsonify
 
-app = Flask(__name__)
+app = Flask (__name__)
 
 app.app_context().push()
 
 @app.route('/')
 def index():
-    return "Index!"
+    return 'Welcome'
 
-@app.route('/is_prime/<num1>', methods=['GET'])
-def is_prime(num1):
+@app.route('/isneg/<i>',methods=['GET'])
+def isneg(i):
+
     try:
-        num1 = int(num1)
-        if num1 > 1:
-            for i in range(2, int(num1/2)+1):
-                if (num1 % i) == 0:
-                    results = { 'msg' : 'False' }
-                    break
-            else:
-                results = { 'msg' : 'True' }
+        try:
+            i = int(i)
+            if i < 0 :
+                resp = { 'msg' : ("True") }
+            elif i >= 0 :
+                resp = { 'msg' : ("False") }
+        except:
+            i = float(i)    
+            if i >= 0 :
+                resp = { 'msg' : ("True 2") }
+            elif i >= 0 :
+                resp = { 'msg' : ("False 2") }      
+                
+    except ValueError:
+        try:
+            i = float(i)
+            resp = { 'msg' : (i+2) }
+        except:
+            i = str(i)
+            resp = { 'msg' : 'error' }
+     
+    return jsonify(resp)   
 
-        else:
-            results = { 'msg' : 'False' }
-
-    except:
-        results = { 'error_msg' : 'inputs must be numbers' }
-    return jsonify(results)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
--- unit_test.py
+++ unit_test.py
@@ -1,16 +1,26 @@
 import unittest
-import app 
+import app
 
-class AppTestCase(unittest.TestCase):
-    def test_is_prime_number17(self):
-        res = app.is_prime(17)
-        data = res.get_json()
-        self.assertEqual(data['msg'], 'True')
+class ApptestCase(unittest.TestCase):
+    def testint5(self):
+        resp = app.isneg(0)
+        data = resp.get_json()
+        self.assertEqual(data['msg'],"False")
 
-    def test_is_prime_number36(self):
-        res = app.is_prime(36)
-        data = res.get_json()
-        self.assertEqual(data['msg'], 'False')
+    def testintnext5(self):
+        resp = app.isneg(3)
+        data = resp.get_json()
+        self.assertEqual(data['msg'],"False")
+
+    def testfloat35(self):
+        resp = app.isneg('-5')
+        data = resp.get_json()
+        self.assertEqual(data['msg'],"True")
+
+    def teststr(self):
+        resp = app.isneg('a')
+        data = resp.get_json()
+        self.assertEqual(data['msg'],'error')
 
 if __name__ == "__main__":
     unittest.main()
